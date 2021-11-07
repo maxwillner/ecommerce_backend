9a6cf3bc1b29c1da989dd53f6803414598edac9c
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-11-07 16:49:07 -0500

    added database connection, added environment variables

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,15 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
